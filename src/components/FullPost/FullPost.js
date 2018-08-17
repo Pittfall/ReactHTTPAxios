e5573324193a9e752fab75c02bea87c348da3f0f
arusu <arusu@sehcomp.ca>
2018-08-17 09:28:59 -0400

    Added the ability to retrieve posts from a dummy server and added the ability to select one

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        selectedPost: null
+    }
 
-        );
+    componentDidUpdate (prevProps) {
+        if (!this.props.postId) {
+            return;
+        }
+
+        if (prevProps.postId === this.props.postId) {
+            return;
+        
+        }
+
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.postId)
+            .then(response => {
+                this.setState({selectedPost : response.data});
+            });
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+
+        if (this.props.postId) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+
+        if (this.state.selectedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.selectedPost.title}</h1>
+                    <p>{this.state.selectedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
         return post;
     }
 }
