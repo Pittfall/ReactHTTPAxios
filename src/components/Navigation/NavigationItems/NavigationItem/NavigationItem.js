240b8ecc0618e201f035e881a9e005dfa124693c
Andrew <andrewrusu87@hotmail.com>
2018-08-25 10:05:19 -0400

    Added Links from react-router so that pages are not reloaded every time as they are done with an anchor tag.  Pages are now just rendered or hidden

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li>
-    <a href={props.link}>{props.children}</a>
+    <Link to={props.link}>{props.children}</Link>
   </li>
 );
 
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import NavigationItems from '../../components/Navigation/NavigationItems/NavigationItems'
 import Posts from '../Posts/Posts'
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -12,6 +13,7 @@ class Blog extends Component {
                 {/* Can render jsx */}
                 {/* <Route path="/" exact render={() => <Posts />} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
